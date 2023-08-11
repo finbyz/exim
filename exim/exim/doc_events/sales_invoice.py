f6212c2b9dbeb2b6d4a9e0beb2ff68f7c3be81e1
Finbyz <info@finbyz.tech>
2023-08-11 13:14:22 +0530

    changes

--- exim/exim/doc_events/sales_invoice.py
+++ exim/exim/doc_events/sales_invoice.py
@@ -14,6 +14,7 @@ def cal_total(self):
         total_packages += flt(d.no_of_packages)
         d.total_tare_weight = flt(d.tare_wt * d.no_of_packages)
         d.gross_wt = flt(d.total_tare_weight) + flt(d.qty)
+        d.fob_value = flt(d.base_amount) - flt(d.freight * self.conversion_rate) - flt(d.insurance * self.conversion_rate)
         total_tare_wt += flt(d.total_tare_weight)
         total_gr_wt += flt(d.gross_wt)
         total_freight += flt(d.freight)
@@ -23,3 +24,5 @@ def cal_total(self):
     self.total_packages = total_packages
     self.total_gr_wt = total_gr_wt
     self.total_tare_wt = total_tare_wt
+    self.freight = total_freight
+    self.insurance = total_insurance
