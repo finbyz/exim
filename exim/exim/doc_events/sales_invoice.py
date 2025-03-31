54b1d56c8aaf19a2c248a9c3faeaa2d265339224
FinByz Tech Pvt. Ltd. <info@finbyz.tech>
2025-03-31 14:49:26 +0530

    Update sales_invoice.py
    PS changes for brc creation

--- exim/exim/doc_events/sales_invoice.py
+++ exim/exim/doc_events/sales_invoice.py
@@ -290,15 +290,16 @@ def create_jv(self):
 
 def create_brc(self):
     if frappe.db.get_value('Address', self.customer_address, 'country') != "India" and frappe.db.exists("DocType", "BRC Management"):
-        brc = frappe.new_doc("BRC Management")
-        brc.invoice_no = self.name
+        
         if not self.is_return and self.shipping_bill_number and self.shipping_bill_date and self.rounded_total:
+	    brc = frappe.new_doc("BRC Management")
+            brc.invoice_no = self.name
             brc.append("shipping_bill_details", {
                 "shipping_bill": self.shipping_bill_number,
                 "shipping_date": self.shipping_bill_date,
                 "shipping_bill_amount": self.rounded_total
             })
-        brc.save(ignore_permissions=True)
+            brc.save(ignore_permissions=True)
 
 
 def cancel_export_lic(self):
