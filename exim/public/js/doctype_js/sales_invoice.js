edcff2d6f2610e5f3e13a372dfb56ea38e96d66f
FinByz Tech Pvt. Ltd. <info@finbyz.tech>
2025-05-31 13:49:46 +0530

    Remove customer filters from shipping address query in sales invoice

--- exim/public/js/doctype_js/sales_invoice.js
+++ exim/public/js/doctype_js/sales_invoice.js
@@ -56,8 +56,7 @@ cur_frm.set_query("customer_address", function () {
 // Shipping Address Filter
 cur_frm.set_query("shipping_address_name", function () {
     return {
-        query: "frappe.contacts.doctype.address.address.address_query",
-        filters: { link_doctype: "Customer", link_name: cur_frm.doc.customer }
+        query: "frappe.contacts.doctype.address.address.address_query"
     };
 });
 
